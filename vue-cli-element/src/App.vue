30eaa14035f0cad19d25e2e85bf46330af95cd31
123 <15236883+xujian123456123@user.noreply.gitee.com>
2025-04-14 20:59:00 +0800

    快速入门ui框架

---
+++ vue-cli-element/src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+//import Button from './Button.vue'
+import Article from './Article.vue'
+</script>
+
+<template>
+  <!-- <Button></Button> -->
+  <Article></Article>
+</template>
+
+
+
---
+++ vue-cli-element/src/Article.vue
@@ -0,0 +1,39 @@
+<template>
+  <el-table :data="tableData" style="width: 100%">
+    <el-table-column prop="date" label="Date" />
+    <el-table-column prop="name" label="Name" />
+    <el-table-column prop="address" label="Address" />
+    <el-table-column prop="name" label="Name" />
+    <el-table-column prop="address" label="Address" width="180">
+      <el-button type="primary" :icon="Edit" circle />
+      <el-button type="danger" :icon="Delete" circle />
+    </el-table-column>
+  </el-table>
+</template>
+
+<script lang="ts" setup>
+const tableData = [
+  {
+    date: '2016-05-03',
+    name: 'Tom',
+    address: 'No. 189, Grove St, Los Angeles'
+  },
+  {
+    date: '2016-05-02',
+    name: 'Tom',
+    address: 'No. 189, Grove St, Los Angeles'
+  },
+  {
+    date: '2016-05-04',
+    name: 'Tom',
+    address: 'No. 189, Grove St, Los Angeles'
+  },
+  {
+    date: '2016-05-01',
+    name: 'Tom',
+    address: 'No. 189, Grove St, Los Angeles'
+  }
+]
+
+import { Check, Delete, Edit, Message, Search, Star } from '@element-plus/icons-vue'
+</script>
